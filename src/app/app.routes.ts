0e7a07569301f55da887f5e28fe8215636da593f
William Castro <wmcastro@bancolombia.com.co>
2025-06-28 08:08:51 -0500

    refactor: change pathnames

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { CourseComponent } from './presentation/pages/course/course.component';
 import { DashboardComponent } from './presentation/pages/dashboard/dashboard.component';
+import { AddCourseComponent } from './presentation/pages/add-course/add-course.component';
+import { UpdateCourseComponent } from './presentation/pages/update-course/update-course.component';
 
 export const routes: Routes = [
   { path: '', component: CourseComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'administracion', component: DashboardComponent },
+  { path: 'agregar', component: AddCourseComponent },
+  { path: 'curso/:id', component: UpdateCourseComponent },
 ];
