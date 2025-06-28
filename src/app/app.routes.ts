b712f3cc5867e7f3b24668ee055b5f1a8938adcf
William Castro <wmcastro@bancolombia.com.co>
2025-06-28 02:01:52 -0500

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CourseComponent } from './presentation/pages/course/course.component';
+import { DashboardComponent } from './presentation/pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CourseComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
