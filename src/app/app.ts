48219a7fe3502441bebe6674116136cacff404a0
William Castro <wmcastro@bancolombia.com.co>
2025-06-28 02:02:37 -0500

    feat: add home template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'course-management-app';
+
+  readonly NETEC_URL = 'https://netec.com.mx/';
+  readonly LOGO_URL =
+    'https://static.wixstatic.com/media/fcaf90_041427bd105c44299c3df778a9b345b9~mv2.png/v1/crop/x_0,y_34,w_1205,h_408/fill/w_502,h_170,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Logo-NETEC-color.png';
 }
