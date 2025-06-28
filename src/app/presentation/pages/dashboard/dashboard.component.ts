ba25348f2fa93ecdecb4aa30031c6bb1cafb9206
William Castro <wmcastro@bancolombia.com.co>
2025-06-28 08:12:15 -0500

    feat: add dashboard page

---
+++ src/app/presentation/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject, signal } from '@angular/core';
+import { TableComponent } from '../../components/table/table.component';
+import { HttpCourseService } from '../../../infrastructure/http-course.service';
+import { CourseParams } from '../../../domain/course.entity';
+import { Router, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { remove } from '../../../store/course.actions';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [TableComponent, RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  private service = inject(HttpCourseService);
+  private router = inject(Router);
+  private store = inject(Store);
+
+  courses = signal<CourseParams[]>([]);
+
+  ngOnInit() {
+    this.getAllCourses();
+  }
+
+  private getAllCourses() {
+    this.service.getAll().subscribe({
+      next: (values) => {
+        this.courses.set(values);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  handleUpdate(id: number) {
+    this.router.navigate(['/curso', id]);
+  }
+
+  handleRemove(id: number) {
+    this.service.delete(id).subscribe({
+      next: () => {
+        this.deleteCourse(id);
+        this.getAllCourses();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  private deleteCourse(id: number) {
+    this.store.dispatch(remove({ id }));
+  }
+}
