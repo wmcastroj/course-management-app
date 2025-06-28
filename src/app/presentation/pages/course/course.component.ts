87b341bff85d33f458884bd4bf04acd6b753d90d
William Castro <wmcastro@bancolombia.com.co>
2025-06-28 02:04:03 -0500

    feat: add home page with course service implementation

---
+++ src/app/presentation/pages/course/course.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { HttpCourseService } from '../../../infrastructure/http-course.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
+import { CourseResponse } from '../../../domain/course.entity';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-course',
+  imports: [CardComponent],
+  templateUrl: './course.component.html',
+  styleUrl: './course.component.css',
+})
+export class CourseComponent {
+  private service = inject(HttpCourseService);
+
+  courses = toSignal(
+    this.service.getAll().pipe(
+      catchError((err) => {
+        console.log(err);
+        return of([]);
+      })
+    ),
+    { initialValue: [] as CourseResponse[] }
+  );
+}
