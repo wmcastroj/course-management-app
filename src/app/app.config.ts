f2496b2ec2a78df12d509a8f202df08247e2efd6
William Castro <wmcastro@bancolombia.com.co>
2025-06-28 02:01:33 -0500

    reafactor:  add providers for ngrx and router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { courseFeatureKey, reducer } from './store/course.reducer';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideHttpClient(),
+    provideState(courseFeatureKey, reducer),
+  ],
 };
